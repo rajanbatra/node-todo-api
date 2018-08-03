3c83f110d57efd4717dd4b961abd3500e973570c
Rajan <batra.rajan97@gmail.com>
2018-08-03 14:24:02 -0500

    Add update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,11 +8,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 	console.log('Connected to MongoDb server');
 	const db = client.db('TodoApp');
 
-	db.collection('Todos').findOneAndUpdate({
-		_id: new ObjectID('5b622df1e30b3901b2654891')
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('5b622df1e30b3901b2654891')
+	// }, {
+	// 	$set: {
+	// 		completed: true
+	// 	}
+	// }, {
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndUpdate({
+		_id: new ObjectID('5b63eeefe30b3901b26577fb')
 	}, {
+		$inc: {
+			age: 1
+		},
 		$set: {
-			completed: true
+			name: 'Rajan'
 		}
 	}, {
 		returnOriginal: false
